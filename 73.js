36922d9ad87a0f371aabc9de8da8c656b6cb3662
aminDB81 <aminbaluchi67@gmail.com>
2023-07-13 18:52:52 +0330

    Add the edit tasks section

--- 73.js
+++ 73.js
@@ -20,16 +20,37 @@ function createListElement() {
   li.addEventListener("click", crossOut);
 
   var dBtn = document.createElement("button");
-  var icon = document.createElement("i");
-  icon.classList.add("fa-solid", "fa-x");
-  dBtn.appendChild(icon);
+  var deleteIcon = document.createElement("i");
+  var editIcon = document.createElement("i");
+  deleteIcon.classList.add("fa-solid", "fa-x");
+  editIcon.classList.add("fa-solid", "fa-pen-to-square");
+  dBtn.appendChild(editIcon);
+  dBtn.appendChild(deleteIcon);
   li.appendChild(dBtn);
 
-  dBtn.addEventListener("click", deleteListItem.bind(null, li)); // Pass the list item as an argument
+  dBtn.addEventListener("click", handleButtonClick); // Attach event listener to the button
+
+  function handleButtonClick(event) {
+    var target = event.target;
+    var listItem = target.closest("li");
+    var deleteIcon = listItem.querySelector(".fa-x");
+    var editIcon = listItem.querySelector(".fa-pen-to-square");
+
+    if (target === deleteIcon) {
+      deleteListItem(listItem);
+    } else if (target === editIcon) {
+      editListItem(listItem);
+    }
+  }
 
   function deleteListItem(listItem) {
     listItem.parentNode.removeChild(listItem); // Remove the list item from the page
     saveTasks();
+  }
+
+  function editListItem(listItem) {
+    // Handle edit functionality here
+    console.log("Edit clicked");
   }
 
   saveTasks();
@@ -72,10 +93,6 @@ function retrieveTasks() {
         li.classList.add("done");
       }
 
-      if (tasks[i].deleted) {
-        li.classList.add("delete");
-      }
-
       function crossOut() {
         this.classList.toggle("done");
         saveTasks();
@@ -83,17 +100,66 @@ function retrieveTasks() {
       li.addEventListener("click", crossOut);
 
       var dBtn = document.createElement("button");
-      var icon = document.createElement("i");
-      icon.classList.add("fa-solid", "fa-x");
-      dBtn.appendChild(icon);
+      var deleteIcon = document.createElement("i");
+      var editIcon = document.createElement("i");
+      deleteIcon.classList.add("fa-solid", "fa-x");
+      editIcon.classList.add("fa-solid", "fa-pen-to-square");
+      dBtn.appendChild(editIcon);
+      dBtn.appendChild(deleteIcon);
       li.appendChild(dBtn);
 
-      dBtn.addEventListener("click", deleteListItem.bind(null, li)); // Pass the list item as an argument
+      dBtn.addEventListener("click", handleButtonClick); // Attach event listener to the button
+
+      function handleButtonClick(event) {
+        var target = event.target;
+        var listItem = target.closest("li");
+        var deleteIcon = listItem.querySelector(".fa-x");
+        var editIcon = listItem.querySelector(".fa-pen-to-square");
+
+        if (target === deleteIcon) {
+          deleteListItem(listItem);
+        } else if (target === editIcon) {
+          editListItem(listItem);
+        }
+      }
 
       function deleteListItem(listItem) {
         listItem.parentNode.removeChild(listItem); // Remove the list item from the page
         saveTasks();
       }
+
+function editListItem(listItem) {
+  var taskText = listItem.firstChild;
+  var editInput = document.createElement("input");
+  editInput.type = "text";
+  editInput.value = taskText.textContent;
+  listItem.replaceChild(editInput, taskText);
+
+  editInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      taskText.textContent = editInput.value;
+      listItem.replaceChild(taskText, editInput);
+      saveTasks();
+    }
+  });
+
+  // Handle click outside the input to update the task value
+  var isEditing = true; // Flag to track if editing is in progress
+
+  editInput.addEventListener("click", function(event) {
+    event.stopPropagation(); // Prevent click event from propagating to document
+    isEditing = true; // Set editing flag to true
+  });
+
+  document.addEventListener("click", function(event) {
+    if (!isEditing) {
+      taskText.textContent = editInput.value;
+      listItem.replaceChild(taskText, editInput);
+      saveTasks();
+    }
+    isEditing = false; // Reset editing flag to false after click event
+  });
+}
     }
   }
 }
